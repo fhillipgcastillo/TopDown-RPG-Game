009002d6bb073002fed95df5d81e8b3781f8ed65
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2025-02-24 23:11:06 -0400

    Added a bit of documentation

--- Assets/InventoryItem.cs
+++ Assets/InventoryItem.cs
@@ -12,21 +12,20 @@ public class InventoryItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     [HideInInspector] public Transform parentAfterDrag;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        image.raycastTarget = false; 
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root );
+        image.raycastTarget = false; // this will take out the raycast target
+        parentAfterDrag = transform.parent; // store the current parent in case of missed drop
+        transform.SetParent(transform.root ); // te the parent as the root, so it can hover over everything
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        transform.position = eventData.position; // move the element based on mouse position on the screen
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        image.raycastTarget = true;
-        transform.SetParent(parentAfterDrag);
-        // transform.localPosition = Vector3.zero;
+        image.raycastTarget = true; //reset the raycast to get the target
+        transform.SetParent(parentAfterDrag); // reset the previous parent on drop (for now)
     }
 
 }

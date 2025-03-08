d24f31726cb1b22fb1ca49dea9b051880c79fd03
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2025-03-08 18:34:53 -0400

    Attaching click event to UI Document for the main Menu Improving a little bit the UI Styling of the main Menu

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -4,17 +4,47 @@ public class MainMenuManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private UIDocument uiDocument;
-
+    public string gameTitleText = "RGP Game";
     private void Start()
     {
         // Assuming you have a way to get the UIDocument reference
         uiDocument = GetComponent<UIDocument>();
 
         // Access an element by name
-        Label myLabel = uiDocument.rootVisualElement.Q<Label>("MyLabelName");
+        Label myLabel = uiDocument.rootVisualElement.Q<Label>("gameTitle");
+        Button playBtn = uiDocument.rootVisualElement.Q<Button>("StartBtn");
+        Button settingsBtn = uiDocument.rootVisualElement.Q<Button>("SettingsBtn");
+        Button exitBtn = uiDocument.rootVisualElement.Q<Button>("ExitBtn");
         if (myLabel != null)
         {
-            myLabel.text = "New Text";
+            myLabel.text = gameTitleText;
         }
+        if (playBtn != null)
+        {
+            playBtn.clicked += playGame;
+        }
+        if (settingsBtn != null)
+        {
+            settingsBtn.clicked += SettingsClicked;
+        }
+        if (exitBtn != null)
+        {
+            exitBtn.clicked += ExitGame;
+        }
+    }
+
+    private void ExitGame()
+    {
+        Debug.Log("Exit game clicked");
+    }
+
+    private void SettingsClicked()
+    {
+        Debug.Log("Settings clicked");
+    }
+
+    private void playGame()
+    {
+        Debug.Log("Play game clicked");
     }
 }
